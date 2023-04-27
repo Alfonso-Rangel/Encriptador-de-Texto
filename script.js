c23632d35c220d354553a34ef4cac0918e44ca04
Alfonso-Rangel <luisrangel187@hotmail.com>
2023-04-26 23:07:58 -0600

    Primera versión del encriptador

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-function textsize(cadena) {
-    alert(cadena);
+let input;
+function prueba() {
+    input = document.getElementById('texto').value;
+    const textarea = document.getElementById('output-copiar');
+    const miVariable = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+    textarea.value = miVariable;
+    ocultarElementos();
 }
+
+function ocultarElementos() {
+    document.getElementById("imagen-copiar").style.display = "none";
+    document.getElementById("mensaje-copiar").style.display = "none";
+    document.getElementById("output-copiar").style.display = "inline";
+    document.getElementById("boton-copiar").style.display = "inline";
+}
+
+
+
